8b7fd28500952e936d0281f15c6e74e71512b731
Ivan <ivwg23@gmail.com>
2025-07-03 22:10:47 +0800

    revert port

--- main.py
+++ main.py
@@ -128,4 +128,4 @@ async def create_token(
     return {"token": token, "expires_days": expires_days}
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=80)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
